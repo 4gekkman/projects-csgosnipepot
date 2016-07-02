0da69e56bdf4b8fe85b558a8e9d110392aab2e4a
4gekkman <4gekkman@gmail.com>
2016-07-02 22:34:39 +0300

    Autosave to

--- Commands/C24_get_trade_offers_via_html.php
+++ Commands/C24_get_trade_offers_via_html.php
@@ -50,7 +50,27 @@
  *    3 - извлечь не закрытые исходящие торговые предложения (sent offers)
  *    4 - извлечь закрытые исходящие торговые предложения (sent offers history)
  *
+ *  Возможные состоятие торгового предложения
+ *  -----------------------------------------
  *
+ *    1   - Invalid
+ *    2   - Active
+ *    3   - Accepted
+ *    4   - Countered
+ *    5   - Expired
+ *    6   - Canceled
+ *    7   - Declined
+ *    8   - InvalidItems
+ *    9   - NeedsConfirmation
+ *    10  - CanceledBySecondFactor
+ *    11  - InEscrow
+ *
+ *  Возможные варианты подтверждения
+ *  --------------------------------
+ *
+ *    0  - Invalid
+ *    1  - Email
+ *    2  - MobileApp
  *
  *
  *
@@ -254,18 +274,362 @@ class C24_get_trade_offers_via_html extends Job { // TODO: добавить "imp
       });
 
       // 4. Извлечь торговые предложения из $html
+      // - Формат результата должен совпадать с форматом при извлечении через API.
+      // - Он должен быть такой:
+      //
+      //    [
+      //      "trade_offers_sent" => [
+      //        0 => [
+      //          tradeofferid            // id торгового предложения
+      //          accountid_other         // id партнёра по торговле (partner из trade url, не путать со steamid)
+      //          message                 // = '' сообщение
+      //          expiration_time         // временная метка, когда это ТП истечёт
+      //          trade_offer_state       // цифра, обозначающая текущее состояние ТП
+      //          is_our_offer            // является ли ТП исходящим (true/false)
+      //          time_created            // временная метка создания ТП
+      //          time_updated            // временная метка обновления ТП
+      //          from_real_time_trade    // = false
+      //          escrow_end_date         // временная метка истечения escrow
+      //          confirmation_method     // = 2 (через мобильный аутентификатор)
+      //          items_to_give => [
+      //            0 => [
+      //              appid
+      //              contextid
+      //              assetid
+      //              classid
+      //              instanceid
+      //              amount
+      //              missing
+      //            ],
+      //            1 => [ ... ]
+      //          ]
+      //          items_to_receive => [ ... ]
+      //        ],
+      //        1 => [ ... ]
+      //      ],
+      //      "trade_offers_received" => [
+      //        0 => [ ... ],
+      //        1 => [ ... ]
+      //      ]
+      //    ]
+      //
       $tradeoffers = call_user_func(function() USE ($html) {
 
-        // 1] Подготовить массив для результатов
-        $tradeoffers = [];
+        // 4.1. Подготовить массив для результатов
+        $tradeoffers = [
+          "trade_offers_sent" => [],
+          "trade_offers_received" => []
+        ];
 
-        // 2] Создать новые объекты DOMDocument и DOMXpath, загрузить в них $html
+        // 4.2. Создать новые объекты DOMDocument и DOMXpath, загрузить в них $html
         $doc = new \DOMDocument();
         $doc->loadHTML($html);
         $xpath = new \DOMXPath($doc);
 
-        // 3]
-        
+        // 4.3. Получить все элементы с торговыми предложениями
+        $tradeOfferElements = $xpath->query('//div[@id[starts-with(.,"tradeofferid_")]]');
+
+        // 4.4. Пробежаться по $tradeOfferElements и наполнить $tradeoffers
+        foreach ($tradeOfferElements as $tradeOfferElement) {
+
+          // 1] Подготовить массив для ТП
+          $tradeoffer = [];
+
+          // 2] tradeofferid
+          $tradeoffer['tradeofferid'] = str_replace('tradeofferid_', '', $tradeOfferElement->getAttribute('id'));
+
+          // 3] accountid_other
+          $secondaryItemsElement = $xpath->query('.//div[contains(@class, "tradeoffer_items secondary")]', $tradeOfferElement)->item(0);
+          $tradeoffer['accountid_other'] = $xpath->query('.//a[@data-miniprofile]/@data-miniprofile', $secondaryItemsElement)->item(0)->nodeValue;
+
+          // 4] message
+          $messageElement = $xpath->query('.//div[contains(@class, "tradeoffer_message")]/div[contains(@class, "quote")]', $tradeOfferElement)->item(0);
+          if(!is_null($messageElement))
+            $tradeoffer['message'] = $messageElement->nodeValue;
+
+          // 5] expiration_time
+          $footerElement = $xpath->query('.//div[contains(@class, "tradeoffer_footer")]', $tradeOfferElement)->item(0);
+          $tradeoffer['expiration_time'] = !empty($footerElement->nodeValue) ? strtotime(str_replace('Offer expires on ', '', $footerElement->nodeValue)) : "";
+
+          // 6] trade_offer_state + confirmation_method + time_updated + escrow_end_date
+
+            // 6.1] Получить $bannerElement
+            $bannerElement = $xpath->query('.//div[contains(@class, "tradeoffer_items_banner")]', $tradeOfferElement)->item(0);
+
+            // 6.2] Active (2)
+            if (is_null($bannerElement))
+              $tradeoffer['trade_offer_state'] = 2;
+
+            // 6.3] Прочие
+            else {
+
+              // 6.3.1] trade_offer_state(9) + confirmation_method(2)
+              if (strpos($bannerElement->nodeValue, 'Awaiting Mobile Confirmation') !== false) {
+                $tradeoffer['trade_offer_state']  = 9;
+                $tradeoffer['confirmation_method'] = 2;
+              }
+
+              // 6.3.2] trade_offer_state(9) + confirmation_method(1)
+              else if (strpos($bannerElement->nodeValue, 'Awaiting Email Confirmation') !== false) {
+                $tradeoffer['trade_offer_state']  = 9;
+                $tradeoffer['confirmation_method'] = 1;
+              }
+
+              // 6.3.3] trade_offer_state(6) + time_updated
+              else if (strpos($bannerElement->nodeValue, 'Trade Offer Canceled') !== false) {
+                $tradeoffer['trade_offer_state'] = 6;
+                $canceledDate = strtotime(str_replace('Trade Offer Canceled ', '', $bannerElement->nodeValue));
+                if($canceledDate !== false)
+                  $tradeoffer['time_updated'] = $canceledDate;
+              }
+
+              // 6.3.4] trade_offer_state(7) + time_updated
+              else if (strpos($bannerElement->nodeValue, 'Trade Declined') !== false) {
+                $tradeoffer['trade_offer_state'] = 7;
+                $declinedDate = strtotime(str_replace('Trade Declined ', '', $bannerElement->nodeValue));
+                if($declinedDate !== false) {
+                  $tradeoffer['time_updated'] = $declinedDate;
+                }
+              }
+
+              // 6.3.5] trade_offer_state(11) + time_updated + escrow_end_date
+              else if (strpos($bannerElement->nodeValue, 'On hold') !== false) {
+
+                // trade_offer_state(11)
+                $tradeoffer['trade_offer_state'] = 11;
+
+                // time_updated
+                $split = explode('.', $bannerElement->nodeValue);
+                $acceptedString = trim($split[0]);
+                $acceptedDate = \DateTime::createFromFormat('M j, Y @ g:ia', str_replace('Trade Accepted ', '', $acceptedString));
+                if ($acceptedDate !== false) {
+                  $tradeoffer['time_updated'] = $acceptedDate->getTimestamp();
+                }
+
+                // escrow_end_date
+                $escrowString = trim($split[1]);
+                $escrowDate = \DateTime::createFromFormat('M j, Y @ g:ia', str_replace('On hold until ', '', $escrowString));
+                if($escrowDate !== false) {
+                  $tradeoffer['escrow_end_date'] = $escrowDate->getTimestamp();
+                }
+
+              }
+
+              // 6.3.6] trade_offer_state(3) + time_updated
+              else if (strpos($bannerElement->nodeValue, 'Trade Accepted') !== false) {
+                $tradeoffer['trade_offer_state'] = 3;
+                $acceptedDate = \DateTime::createFromFormat('j M, Y @ g:ia', str_replace('Trade Accepted ', '', trim($bannerElement->nodeValue)));
+                if ($acceptedDate !== false) {
+                  $tradeoffer['time_updated'] = $acceptedDate->getTimestamp();
+                }
+              }
+
+              // 6.3.7] trade_offer_state(8)
+              else if (strpos($bannerElement->nodeValue, 'Items Now Unavailable For Trade') !== false) {
+                $tradeoffer['trade_offer_state'] = 8;
+              }
+
+              // 6.3.8] trade_offer_state(4) + time_updated
+              else if (strpos($bannerElement->nodeValue, 'Counter Offer Made') !== false) {
+                $tradeoffer['trade_offer_state'] = 4;
+                $counteredDate = strtotime(str_replace('Counter Offer Made ', '', $bannerElement->nodeValue));
+                if ($counteredDate !== false) {
+                  $tradeoffer['time_updated'] = $counteredDate;
+                }
+              }
+
+              // 6.3.9] trade_offer_state(5) + time_updated
+              else if (strpos($bannerElement->nodeValue, 'Trade Offer Expired') !== false) {
+                $tradeoffer['trade_offer_state'] = 5;
+                $expiredDate = strtotime(str_replace('Trade Offer Expired ', '', $bannerElement->nodeValue));
+                if ($expiredDate !== false) {
+                  $tradeoffer['time_updated'] = $expiredDate;
+                }
+              }
+
+              // 6.3.10] trade_offer_state(1)
+              else {
+                $tradeoffer['trade_offer_state'] = 1;
+              }
+
+            }
+
+          // 7] is_our_offer
+          $tradeoffer['is_our_offer'] = ($this->data['mode'] == 1 || $this->data['mode'] == 2) ? true : false;
+
+          // 8] time_created
+          $tradeoffer['is_our_offer'] = "";
+
+          // 9] from_real_time_trade
+          $tradeoffer['is_our_offer'] = false;
+
+          // 10] items_to_give
+
+
+
+          // 11] items_to_receive
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+        // 4.n. Вернуть результаты
+        return $tradeoffers;
+
+
+
+
+
+
+        /** @var \DOMElement[] $tradeOfferElements */
+        $tradeOfferElements = $xpath->query('//div[@id[starts-with(.,"tradeofferid_")]]');
+        foreach ($tradeOfferElements as $tradeOfferElement) {
+            $tradeOffer = new TradeOffer();
+            $tradeOffer->setIsOurOffer($isOurOffer);
+            $tradeOfferId = str_replace('tradeofferid_', '', $tradeOfferElement->getAttribute('id'));
+            $tradeOffer->setTradeOfferId($tradeOfferId);
+            $primaryItemsElement = $xpath->query('.//div[contains(@class, "tradeoffer_items primary")]', $tradeOfferElement)->item(0);
+            $itemsToGiveList = $xpath->query('.//div[contains(@class, "tradeoffer_item_list")]/div[contains(@class, "trade_item")]', $primaryItemsElement);
+            $itemsToGive = [];
+            /** @var \DOMElement[] $itemsToGiveList */
+            foreach ($itemsToGiveList as $itemToGive) {
+                //classinfo/570/583164181/93973071
+                //         appId/classId/instanceId
+                //570/2/7087209304/76561198045552709
+                //appId/contextId/assetId/steamId
+                $item = new TradeOffer\Item();
+                $itemInfo = explode('/', $itemToGive->getAttribute('data-economy-item'));
+                if ($itemInfo[0] == 'classinfo') {
+                    $item->setAppId($itemInfo[1]);
+                    $item->setClassId($itemInfo[2]);
+                    if (isset($itemInfo[3])) {
+                        $item->setInstanceId($itemInfo[3]);
+                    }
+                } else {
+                    $item->setAppId($itemInfo[0]);
+                    $item->setContextId($itemInfo[1]);
+                    $item->setAssetId($itemInfo[2]);
+                }
+                if (strpos($itemToGive->getAttribute('class'), 'missing') !== false) {
+                    $item->setMissing(true);
+                }
+                $itemsToGive[] = $item;
+            }
+            $tradeOffer->setItemsToGive($itemsToGive);
+            $secondaryItemsElement = $xpath->query('.//div[contains(@class, "tradeoffer_items secondary")]', $tradeOfferElement)->item(0);
+            $otherAccountId = $xpath->query('.//a[@data-miniprofile]/@data-miniprofile', $secondaryItemsElement)->item(0)->nodeValue;
+            $tradeOffer->setOtherAccountId($otherAccountId);
+            $itemsToReceiveList = $xpath->query('.//div[contains(@class, "tradeoffer_item_list")]/div[contains(@class, "trade_item")]', $secondaryItemsElement);
+            $itemsToReceive = [];
+            /** @var \DOMElement[] $itemsToReceiveList */
+            foreach ($itemsToReceiveList as $itemToReceive) {
+                $item = new TradeOffer\Item();
+                $itemInfo = explode('/', $itemToReceive->getAttribute('data-economy-item'));
+                if ($itemInfo[0] == 'classinfo') {
+                    $item->setAppId($itemInfo[1]);
+                    $item->setClassId($itemInfo[2]);
+                    if (isset($itemInfo[3])) {
+                        $item->setInstanceId($itemInfo[3]);
+                    }
+                } else {
+                    $item->setAppId($itemInfo[0]);
+                    $item->setContextId($itemInfo[1]);
+                    $item->setAssetId($itemInfo[2]);
+                }
+                if (strpos($itemToReceive->getAttribute('class'), 'missing') !== false) {
+                    $item->setMissing(true);
+                }
+                $itemsToReceive[] = $item;
+            }
+            $tradeOffer->setItemsToReceive($itemsToReceive);
+            // message
+            $messageElement = $xpath->query('.//div[contains(@class, "tradeoffer_message")]/div[contains(@class, "quote")]', $tradeOfferElement)->item(0);
+            if (!is_null($messageElement)) {
+                $tradeOffer->setMessage($messageElement->nodeValue);
+            }
+            // expiration
+            $footerElement = $xpath->query('.//div[contains(@class, "tradeoffer_footer")]', $tradeOfferElement)->item(0);
+            if (!empty($footerElement->nodeValue)) {
+                $expirationTimeString = str_replace('Offer expires on ', '', $footerElement->nodeValue);
+                $tradeOffer->setExpirationTime(strtotime($expirationTimeString));
+            }
+            // state
+            $bannerElement = $xpath->query('.//div[contains(@class, "tradeoffer_items_banner")]', $tradeOfferElement)->item(0);
+            if (is_null($bannerElement)) {
+                $tradeOffer->setTradeOfferState(TradeOffer\State::Active);
+            } else {
+                if (strpos($bannerElement->nodeValue, 'Awaiting Mobile Confirmation') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::NeedsConfirmation);
+                    $tradeOffer->setConfirmationMethod(TradeOffer\ConfirmationMethod::MobileApp);
+                } else if (strpos($bannerElement->nodeValue, 'Awaiting Email Confirmation') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::NeedsConfirmation);
+                    $tradeOffer->setConfirmationMethod(TradeOffer\ConfirmationMethod::Email);
+                } else if (strpos($bannerElement->nodeValue, 'Trade Offer Canceled') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::Canceled);
+                    $canceledDate = strtotime(str_replace('Trade Offer Canceled ', '', $bannerElement->nodeValue));
+                    if ($canceledDate !== false) {
+                        $tradeOffer->setTimeUpdated($canceledDate);
+                    }
+                } else if (strpos($bannerElement->nodeValue, 'Trade Declined') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::Declined);
+                    $declinedDate = strtotime(str_replace('Trade Declined ', '', $bannerElement->nodeValue));
+                    if ($declinedDate !== false) {
+                        $tradeOffer->setTimeUpdated($declinedDate);
+                    }
+                } else if (strpos($bannerElement->nodeValue, 'On hold') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::InEscrow);
+                    $split = explode('.', $bannerElement->nodeValue);
+                    $acceptedString = trim($split[0]);
+                    $acceptedDate = \DateTime::createFromFormat('M j, Y @ g:ia', str_replace('Trade Accepted ', '', $acceptedString));
+                    if ($acceptedDate !== false) {
+                        $tradeOffer->setTimeUpdated($acceptedDate->getTimestamp());
+                    }
+                    $escrowString = trim($split[1]);
+                    $escrowDate = \DateTime::createFromFormat('M j, Y @ g:ia', str_replace('On hold until ', '', $escrowString));
+                    if ($escrowDate !== false) {
+                        $tradeOffer->setEscrowEndDate($escrowDate->getTimestamp());
+                    }
+                } else if (strpos($bannerElement->nodeValue, 'Trade Accepted') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::Accepted);
+                    // 14 Dec, 2015 @ 4:32am
+                    $acceptedDate = \DateTime::createFromFormat('j M, Y @ g:ia', str_replace('Trade Accepted ', '', trim($bannerElement->nodeValue)));
+                    if ($acceptedDate !== false) {
+                        $tradeOffer->setTimeUpdated($acceptedDate->getTimestamp());
+                    }
+                } else if (strpos($bannerElement->nodeValue, 'Items Now Unavailable For Trade') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::InvalidItems);
+                } else if (strpos($bannerElement->nodeValue, 'Counter Offer Made') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::Countered);
+                    $counteredDate = strtotime(str_replace('Counter Offer Made ', '', $bannerElement->nodeValue));
+                    if ($counteredDate !== false) {
+                        $tradeOffer->setTimeUpdated($counteredDate);
+                    }
+                } else if (strpos($bannerElement->nodeValue, 'Trade Offer Expired') !== false) {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::Expired);
+                    $expiredDate = strtotime(str_replace('Trade Offer Expired ', '', $bannerElement->nodeValue));
+                    if ($expiredDate !== false) {
+                        $tradeOffer->setTimeUpdated($expiredDate);
+                    }
+                } else {
+                    $tradeOffer->setTradeOfferState(TradeOffer\State::Invalid);
+                }
+            }
+            $tradeOffers[] = $tradeOffer;
+        }
+        return $tradeOffers;
+
+
 
 
 
