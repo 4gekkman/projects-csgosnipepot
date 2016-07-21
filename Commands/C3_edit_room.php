6b3af1f30c72b26bbc91f24537be737bdbc80c31
4gekkman <4gekkman@gmail.com>
2016-07-21 19:41:22 +0300

    Autosave to

--- Commands/C3_edit_room.php
+++ Commands/C3_edit_room.php
@@ -7,14 +7,14 @@
 /**
  *  Что делает
  *  ----------
- *    - Create a new game room
+ *    - Edit the specified room
  *
  *  Какие аргументы принимает
  *  -------------------------
  *
  *    [
  *      "data" => [
- *        name
+ *
  *      ]
  *    ]
  *
@@ -101,7 +101,7 @@
 //---------//
 // Команда //
 //---------//
-class C2_create_new_room extends Job { // TODO: добавить "implements ShouldQueue" - и команда будет добавляться в очередь задач
+class C3_edit_room extends Job { // TODO: добавить "implements ShouldQueue" - и команда будет добавляться в очередь задач
 
   //----------------------------//
   // А. Подключить пару трейтов //
@@ -136,210 +136,99 @@ class C2_create_new_room extends Job { // TODO: добавить "implements Sho
      * Оглавление
      *
      *  1. Провести валидацию входящих параметров
-     *  2. Проверить, нет ли уже комнаты с таким именем
-     *  3. Получить все параметры по умолчанию для новых комнат из конфига
-     *  4. Создать новую комнату с параметрами по умолчанию
+     *  2. Попробовать найти комнату с таким ID
+     *  3. Попробовать найти комнату с таким именем
+     *  4. Внести изменения в $room2edit
+     *  5. Сделать commit
+     *  6. Вернуть результаты
      *
      *  N. Вернуть статус 0
      *
      */
 
-    //----------------------------------------------------------//
-    // Создать новую игровую комнату с параметрами по умолчанию //
-    //----------------------------------------------------------//
+    //------------------//
+    // Изменить комнату //
+    //------------------//
     $res = call_user_func(function() { try { DB::beginTransaction();
 
       // 1. Провести валидацию входящих параметров
       $validator = r4_validate($this->data, [
-        "name"              => ["required", "string"],
+
+        "id"                        => ["required", "regex:/^[1-9]+[0-9]*$/ui"],
+        "is_on" 									  => ["required", "regex:/^[01]{1}$/ui"],
+        "bet_accepting_mode"  		  => ["required", "in:roll,availability"],
+        "name" 									    => ["required", "string"],
+        "room_round_duration_sec"   => ["required", "regex:/^[0-9]+$/ui"],
+        "max_items_per_bet" 			  => ["required", "regex:/^[0-9]+$/ui"],
+        "max_items_per_round" 		  => ["required", "regex:/^[0-9]+$/ui"],
+        "min_items_per_bet" 			  => ["required", "regex:/^[0-9]+$/ui"],
+        "min_items_per_round" 		  => ["required", "regex:/^[0-9]+$/ui"],
+        "min_bet" 								  => ["required", "regex:/^[0-9]+$/ui"],
+        "max_bet" 								  => ["required", "regex:/^[0-9]+$/ui"],
+        "min_bet_round" 					  => ["required", "regex:/^[0-9]+$/ui"],
+        "max_bet_round" 					  => ["required", "regex:/^[0-9]+$/ui"],
+        "allow_unstable_prices" 	  => ["required", "regex:/^[01]{1}$/ui"],
+        "allow_only_types" 			    => ["required", "json"],
+        "fee_percents" 	            => ["required", "r4_between:0,100"],
+        "change" 				            => ["required", "regex:/^[01]{1}$/ui"],
+        "one_bot_payout"            => ["required", "regex:/^[01]{1}$/ui"],
+        "payout_limit_min"          => ["required", "regex:/^[1-9]+[0-9]*$/ui"],
+
       ]); if($validator['status'] == -1) {
+
         throw new \Exception($validator['data']);
+
       }
 
-      // 2. Проверить, нет ли уже комнаты с таким именем
-      $room_check = \M9\Models\MD1_rooms::where('name', $this->data['name'])->first();
-      if(!empty($room_check))
-        throw new \Exception('There is a room with such name.');
+      // 2. Попробовать найти комнату с таким именем
+      $room2edit = \M9\Models\MD1_rooms::find($this->data['id']);
+      if(empty($room2edit))
+        throw new \Exception('A room with id == '.$this->data['id'].' not found in the system.');
 
-      // 3. Получить все параметры по умолчанию для новых комнат из конфига
-      $defaults = [];
+      // 3. Попробовать найти комнату с таким именем
+      $room2edit_byname = \M9\Models\MD1_rooms::where('name', $this->data['name'])->first();
+      if(!empty($room2edit_byname) && $room2edit->id != $room2edit_byname->id)
+        throw new \Exception('A room with name == '.$this->data['name'].' already present in the system.');
 
-        // 1] Режим приёма ставок комнаты
-        $defaults['bet_accepting_mode'] = config('M9.bet_accepting_mode');
-        if(empty($defaults['bet_accepting_mode'])) $defaults['bet_accepting_mode'] = 'availability';
+      // 4. Внести изменения в $room2edit
 
-        // 2] Длительность раундов в комнате
-        $defaults['room_round_duration_sec'] = config('M9.room_round_duration_sec');
-        if(empty($defaults['room_round_duration_sec'])) $defaults['room_round_duration_sec'] = '35';
+        // 4.1. Внести основные изменения
+        foreach($this->data as $key => $value) {
 
-        // 3] MAX кол-во вещей в 1-й ставке в раундах комнады
-        $defaults['max_items_per_bet'] = config('M9.max_items_per_bet');
-        if(empty($defaults['max_items_per_bet'])) $defaults['max_items_per_bet'] = '0';
+          // Если $key == 'timestamp', продолжить
+          if($key == 'timestamp') continue;
 
-        // 4] MIN кол-во вещей в 1-й ставке в раундах комнады
-        $defaults['min_items_per_bet'] = config('M9.min_items_per_bet');
-        if(empty($defaults['min_items_per_bet'])) $defaults['min_items_per_bet'] = '0';
+          // Если $key == 'id', продолжить
+          if($key == 'id') continue;
 
-        // 5] MAX кол-во вещей в раунде
-        $defaults['max_items_per_round'] = config('M9.max_items_per_round');
-        if(empty($defaults['max_items_per_round'])) $defaults['max_items_per_round'] = '0';
+          // Если $key == 'avatar_steam', продолжить
+          if($key == 'avatar_steam') continue;
 
-        // 6] MIN кол-во вещей в раунде
-        $defaults['min_items_per_round'] = config('M9.min_items_per_round');
-        if(empty($defaults['min_items_per_round'])) $defaults['min_items_per_round'] = '0';
-
-        // 7] MAX размер ставки в $
-        $defaults['max_bet'] = config('M9.max_bet');
-        if(empty($defaults['max_bet'])) $defaults['max_bet'] = '0';
-
-        // 8] MIN размер ставки в $
-        $defaults['min_bet'] = config('M9.min_bet');
-        if(empty($defaults['min_bet'])) $defaults['min_bet'] = '0';
-
-        // 9] MAX общий размер ставок в $ в раунде
-        $defaults['max_bet_round'] = config('M9.max_bet_round');
-        if(empty($defaults['max_bet_round'])) $defaults['max_bet_round'] = '0';
-
-        // 10] MIN общий размер ставок в $ в раунде
-        $defaults['min_bet_round'] = config('M9.min_bet_round');
-        if(empty($defaults['min_bet_round'])) $defaults['min_bet_round'] = '0';
-
-        // 11] Разрешить ли делать ставки вещами с нестабильными ценами
-        $defaults['allow_unstable_prices'] = config('M9.allow_unstable_prices');
-        if(empty($defaults['allow_unstable_prices'])) $defaults['allow_unstable_prices'] = 0;
-
-        // 12] Разрешить принимать в виде ставок только эти типы вещей
-
-          // 12.1] Доступные типы вещей
-          $defaults['available_types_of_items'] = config('M9.available_types_of_items');
-          if(empty($defaults['available_types_of_items'])) $defaults['available_types_of_items'] = [
-            'case',
-            'key',
-            'startrak',
-            'souvenir packages',
-            'souvenir',
-            'knife',
-            'weapon'
-          ];
-
-          // 12.2] Разрешить принимать в виде ставок только эти типы вещей
-          $defaults['allow_only_types'] = config('M9.allow_only_types');
-          if(empty($defaults['allow_only_types'])) $defaults['allow_only_types'] = [
-            'case',
-            'key',
-            'startrak',
-            'souvenir packages',
-            'knife',
-            'weapon'
-          ];
-
-        // 13] Размер комиссии сервиса в комнате в % от банка
-        $defaults['fee_percents'] = config('M9.fee_percents');
-        if(empty($defaults['fee_percents'])) $defaults['fee_percents'] = '0';
-
-        // 14] Включить ли механизм размена при выплате выигрышей
-        $defaults['change'] = config('M9.change');
-        if(empty($defaults['change'])) $defaults['change'] = 1;
-
-        // 15] Включить ли механизм выплат выигрышей от имени 1-го бота
-        $defaults['one_bot_payout'] = config('M9.one_bot_payout');
-        if(empty( $defaults['one_bot_payout']))  $defaults['one_bot_payout'] = 0;
-
-        // 16] Лимит в минутах на то, чтобы победитель забрал выигрыш
-        $defaults['payout_limit_min'] = config('M9.payout_limit_min');
-        if(empty($defaults['payout_limit_min'])) $defaults['payout_limit_min'] = '60';
-
-      // 4. Провести валидацию всех параметров по умолчанию для новых комнат
-
-        // 4.1. Подготовить массив-конфиг валидатора
-        $validator_arr = [
-
-          "bet_accepting_mode"        => ["required", "in:roll,availability"],
-          "room_round_duration_sec"   => ["required", "regex:/^[0-9]+$/ui"],
-          "max_items_per_bet"         => ["required", "regex:/^[0-9]+$/ui"],
-          "min_items_per_bet"         => ["required", "regex:/^[0-9]+$/ui"],
-          "max_items_per_round"       => ["required", "regex:/^[0-9]+$/ui"],
-          "min_items_per_round"       => ["required", "regex:/^[0-9]+$/ui"],
-          "max_bet"                   => ["required", "regex:/^[0-9]+$/ui"],
-          "min_bet"                   => ["required", "regex:/^[0-9]+$/ui"],
-          "max_bet_round"             => ["required", "regex:/^[0-9]+$/ui"],
-          "min_bet_round"             => ["required", "regex:/^[0-9]+$/ui"],
-          "allow_unstable_prices"     => ["required", "regex:/^[01]{1}$/ui"],
-          "available_types_of_items"  => ["required", "array"],
-          "allow_only_types"          => ["required", "array"],
-          "fee_percents"              => ["required", "r4_between:0,100"],
-          "change"                    => ["required", "regex:/^[01]{1}$/ui"],
-          "one_bot_payout"            => ["required", "regex:/^[01]{1}$/ui"],
-          "payout_limit_min"          => ["required", "regex:/^[1-9]+[0-9]*$/ui"],
-
-        ];
-
-        // 4.2. Дополнить $validator_arr значением для allow_only_types.*
-        call_user_func(function() USE (&$validator_arr, $defaults) {
-
-          // 1] Подготовить значение
-          $value = 'in:';
-
-          // 2] Наполнить $value
-          for($i=0; $i<count($defaults['available_types_of_items']); $i++) {
-            if($i != +count($defaults['available_types_of_items']) - 1) {
-              $value = $value . $defaults['available_types_of_items'][$i] . ',';
-            }
-            else {
-              $value = $value . $defaults['available_types_of_items'][$i];
-            }
-          }
-          write2log($value, []);
-          // 3] Добавить значение в $validator_arr
-          $validator_arr["allow_only_types.*"] = ["required", $value];
-
-        });
-
-        // 4.3. Провести валидацию
-        $validator = r4_validate($defaults, $validator_arr); if($validator['status'] == -1) {
-
-          throw new \Exception($validator['data']);
+          // В общем случае
+          $room2edit[$key] = $value;
 
         }
 
-      // 5. Создать новую комнату с параметрами по умолчанию
+        // 4.n. Сохранить
+        $room2edit->save();
 
-        // 5.1. Создать новую модель комнаты
-        $room = new \M9\Models\MD1_rooms();
+      // 5. Сделать commit
+      DB::commit();
 
-        // 5.2. Наполнить её поля
-        foreach($defaults as $key => $value) {
-
-          // 1] Если $key = available_types_of_items
-          if($key == 'available_types_of_items') continue;
-
-          // 2] Если $key = allow_only_types
-          if($key == 'allow_only_types') {
-            $room[$key] = json_encode($value, JSON_UNESCAPED_UNICODE);
-            continue;
-          }
-
-          // n] В общем случае
-          $room[$key] = $value;
-
-        }
-
-        // 5.3. Добавить имя комнаты
-        $my_mb_ucfirst = function($str) {
-            $fc = mb_strtoupper(mb_substr($str, 0, 1));
-            return $fc.mb_substr($str, 1);
-        };
-        $room['name'] = $my_mb_ucfirst(mb_strtolower($this->data['name']));
-
-        // 5.4. Сохранить созданную модель
-        $room->save();
+      // 6. Вернуть результаты
+      return [
+        "status"  => 0,
+        "data"    => [
+          "id"      => $room2edit->id
+        ]
+      ];
 
 
     DB::commit(); } catch(\Exception $e) {
-        $errortext = 'Invoking of command C2_create_new_room from M-package M9 have ended on line "'.$e->getLine().'" on file "'.$e->getFile().'" with error: '.$e->getMessage();
+        $errortext = 'Invoking of command C3_edit_room from M-package M9 have ended on line "'.$e->getLine().'" on file "'.$e->getFile().'" with error: '.$e->getMessage();
         DB::rollback();
         Log::info($errortext);
-        write2log($errortext, ['M9', 'C2_create_new_room']);
+        write2log($errortext, ['M9', 'C3_edit_room']);
         return [
           "status"  => -2,
           "data"    => [
