f1548f455d8978fb44bc11af7d19f9260523abe9
4gekkman <4gekkman@gmail.com>
2016-11-27 07:36:19 +0300

    Autosave to

--- Commands/C7_get_all_game_data.php
+++ Commands/C7_get_all_game_data.php
@@ -516,8 +516,6 @@ class C7_get_all_game_data extends Job { // TODO: добавить "implements S
 
       });
 
-      //Log::info($rooms);
-
       // n. Вернуть результаты
       DB::commit();
       return [
