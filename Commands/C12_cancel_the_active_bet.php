3df78c0ee882fa3ee36e64aa4bea513f99d2b634
4gekkman <4gekkman@gmail.com>
2016-12-05 23:17:03 +0300

    Autosave to

--- Commands/C12_cancel_the_active_bet.php
+++ Commands/C12_cancel_the_active_bet.php
@@ -169,105 +169,129 @@ class C12_cancel_the_active_bet extends Job { // TODO: добавить "impleme
         throw new \Exception($validator['data']);
       }
 
-      // 2. Попробовать получить оффер с tradeofferid из Steam через HTTP
-      // - Что означают коды:
-      //
-      //    -3    // Не удалось получить ответ от Steam
-      //    -2    // Информация об отправленных офферах отсутствует в ответе в принципе
-      //    -1    // Среди отправленных офферов не удалось найти оффер с tradeofferid
-      //    0     // Успех, найденный оффер доступен по ключу offer
-      //
-      // $offers = runcommand('\M8\Commands\C19_get_tradeoffers_via_api', ["id_bot"=>3,"activeonly"=>1]);
-      // $offers = runcommand('\M8\Commands\C24_get_trade_offers_via_html', ["id_bot"=>3,"mode"=>3]);
-      $offers_http = call_user_func(function(){
+//      // 2. Попробовать получить оффер с tradeofferid из Steam через HTTP
+//      // - Что означают коды:
+//      //
+//      //    -3    // Не удалось получить ответ от Steam
+//      //    -2    // Информация об отправленных офферах отсутствует в ответе в принципе
+//      //    -1    // Среди отправленных офферов не удалось найти оффер с tradeofferid
+//      //    0     // Успех, найденный оффер доступен по ключу offer
+//      //
+//      // $offers = runcommand('\M8\Commands\C19_get_tradeoffers_via_api', ["id_bot"=>3,"activeonly"=>1]);
+//      // $offers = runcommand('\M8\Commands\C24_get_trade_offers_via_html', ["id_bot"=>3,"mode"=>3]);
+//      $offers_http = call_user_func(function(){
+//
+//        // 1] Получить все активные офферы бота id_bot
+//        $offers = runcommand('\M8\Commands\C24_get_trade_offers_via_html', ["id_bot"=>3,"mode"=>3]);
+//
+//        // 2] Если получить ответ от Steam не удалось
+//        if($offers['status'] != 0)
+//          return [
+//            "code"   => -3,
+//            "offer"  => ""
+//          ];
+//
+//        // 3] Если trade_offers_sent отсутствуют в ответе
+//        if(!array_key_exists('trade_offers_sent', $offers['data']['tradeoffers']))
+//          return [
+//            "code"   => -2,
+//            "offer"  => ""
+//          ];
+//
+//        // 4] Найти среди $offers оффер с tradeofferid
+//
+//          // 4.1] Попробовать найти
+//          for($i=0; $i<count($offers['data']['tradeoffers']['trade_offers_sent']); $i++) {
+//            if($offers['data']['tradeoffers']['trade_offers_sent'][$i]['tradeofferid'] == $this->data['tradeofferid'])
+//              return [
+//                "code"  => 0,
+//                "offer" => $offers['data']['tradeoffers']['trade_offers_sent'][$i]
+//              ];
+//          }
+//
+//          // 4.2] Если найти не удалось
+//          return [
+//            "code"  => -1,
+//            "offer" => ""
+//          ];
+//
+//      });
+//
+//      // 3. Получить из кэша информацию о состоянии активных офферов из m9.C35
+//      // - В формате:
+//      //
+//      //    [
+//      //      [
+//      //        id_bot
+//      //        tradeoffer
+//      //        tradeofferid
+//      //        id_status_old
+//      //        id_status_new
+//      //      ],
+//      //      ...
+//      //    ]
+//      //
+//      $c35_executing = json_decode(Cache::get('m9:processing:bets_ex_active'), true);
+//      if(empty($c35_executing)) $c35_executing = [];
+//
+//      // 4. Попробовать определить текущее состояние оффера tradeofferid
+//      // - Если не удалось, вернуть 0.
+//      $current_tradeofferid_state = call_user_func(function() USE ($offers_http, $c35_executing) {
+//
+//        // 1] Если запрос $offers_http был успешен, вернуть результат
+//        if($offers_http['code'] == 0) {
+//          if(array_key_exists('trade_offer_state', $offers_http['offer']))
+//            return $offers_http['offer']['trade_offer_state'];
+//        }
+//
+//        // 2] В противном случае, попробовать найти tradeofferid в $c35_executing
+//        foreach($c35_executing as $to) {
+//          if($to['tradeofferid'] == $this->data['tradeofferid'])
+//            return $to['id_status_new'];
+//        }
+//
+//        // 3] Если до сих пор ничего не найдено, вернуть 0
+//        return 0;
+//
+//      });
+//
+//      // 5. Если текущее состояние удалось определить, и оно не "Accepted"
+//      if(!empty($current_tradeofferid_state) && $current_tradeofferid_state != 0 && $current_tradeofferid_state != 3) {
+//
+//        // 5.1. Попробовать отменить этот оффер, если он активен
+//        if($current_tradeofferid_state == 2)
+//          $cancel_result = runcommand('\M8\Commands\C27_cancel_trade_offer', [
+//            "id_bot"        => $this->data['id_bot'],
+//            "id_tradeoffer" => $this->data['tradeofferid']
+//          ]);
+//
+//        // 5.2. Если это был активный оффер, и отменить его успешно удалось
+//        // - Внести соответствующие изменения в БД.
+//        if($current_tradeofferid_state == 2 && !empty($cancel_result) && $cancel_result['status'] == 0) {
+//
+//          $result = runcommand('\M9\Commands\C15_cancel_the_active_bet_dbpart', [
+//            "betid"             => $this->data['betid'],
+//            "tradeofferid"      => $this->data['tradeofferid'],
+//            "another_status_id" => 6,
+//            "id_user"           => $this->data['id_user'],
+//            "id_room"           => $this->data['id_room'],
+//          ]);
+//          if($result['status'] != 0)
+//            throw new \Exception($result['data']['errormsg']);
+//
+//        }
+//
+//      }
 
-        // 1] Получить все активные офферы бота id_bot
-        $offers = runcommand('\M8\Commands\C24_get_trade_offers_via_html', ["id_bot"=>3,"mode"=>3]);
+        // 2. Попробовать отменить оффер tradeofferid
+        $cancel_result = runcommand('\M8\Commands\C27_cancel_trade_offer', [
+          "id_bot"        => $this->data['id_bot'],
+          "id_tradeoffer" => $this->data['tradeofferid']
+        ]);
 
-        // 2] Если получить ответ от Steam не удалось
-        if($offers['status'] != 0)
-          return [
-            "code"   => -3,
-            "offer"  => ""
-          ];
-
-        // 3] Если trade_offers_sent отсутствуют в ответе
-        if(!array_key_exists('trade_offers_sent', $offers['data']['tradeoffers']))
-          return [
-            "code"   => -2,
-            "offer"  => ""
-          ];
-
-        // 4] Найти среди $offers оффер с tradeofferid
-
-          // 4.1] Попробовать найти
-          for($i=0; $i<count($offers['data']['tradeoffers']['trade_offers_sent']); $i++) {
-            if($offers['data']['tradeoffers']['trade_offers_sent'][$i]['tradeofferid'] == $this->data['tradeofferid'])
-              return [
-                "code"  => 0,
-                "offer" => $offers['data']['tradeoffers']['trade_offers_sent'][$i]
-              ];
-          }
-
-          // 4.2] Если найти не удалось
-          return [
-            "code"  => -1,
-            "offer" => ""
-          ];
-
-      });
-
-      // 3. Получить из кэша информацию о состоянии активных офферов из m9.C35
-      // - В формате:
-      //
-      //    [
-      //      [
-      //        id_bot
-      //        tradeoffer
-      //        tradeofferid
-      //        id_status_old
-      //        id_status_new
-      //      ],
-      //      ...
-      //    ]
-      //
-      $c35_executing = json_decode(Cache::get('m9:processing:bets_ex_active'), true);
-      if(empty($c35_executing)) $c35_executing = [];
-
-      // 4. Попробовать определить текущее состояние оффера tradeofferid
-      // - Если не удалось, вернуть 0.
-      $current_tradeofferid_state = call_user_func(function() USE ($offers_http, $c35_executing) {
-
-        // 1] Если запрос $offers_http был успешен, вернуть результат
-        if($offers_http['code'] == 0) {
-          if(array_key_exists('trade_offer_state', $offers_http['offer']))
-            return $offers_http['offer']['trade_offer_state'];
-        }
-
-        // 2] В противном случае, попробовать найти tradeofferid в $c35_executing
-        foreach($c35_executing as $to) {
-          if($to['tradeofferid'] == $this->data['tradeofferid'])
-            return $to['id_status_new'];
-        }
-
-        // 3] Если до сих пор ничего не найдено, вернуть 0
-        return 0;
-
-      });
-
-      // 5. Если текущее состояние удалось определить, и оно не "Accepted"
-      if(!empty($current_tradeofferid_state) && $current_tradeofferid_state != 0 && $current_tradeofferid_state != 3) {
-
-        // 5.1. Попробовать отменить этот оффер, если он активен
-        if($current_tradeofferid_state == 2)
-          $cancel_result = runcommand('\M8\Commands\C27_cancel_trade_offer', [
-            "id_bot"        => $this->data['id_bot'],
-            "id_tradeoffer" => $this->data['tradeofferid']
-          ]);
-
-        // 5.2. Если это был активный оффер, и отменить его успешно удалось
+        // 3. Если отменить его успешно удалось
         // - Внести соответствующие изменения в БД.
-        if($current_tradeofferid_state == 2 && !empty($cancel_result) && $cancel_result['status'] == 0) {
+        if(!empty($cancel_result) && $cancel_result['status'] == 0) {
 
           $result = runcommand('\M9\Commands\C15_cancel_the_active_bet_dbpart', [
             "betid"             => $this->data['betid'],
@@ -280,8 +304,7 @@ class C12_cancel_the_active_bet extends Job { // TODO: добавить "impleme
             throw new \Exception($result['data']['errormsg']);
 
         }
-
-      }
+      
 
     DB::commit(); } catch(\Exception $e) {
         $errortext = 'Invoking of command C12_cancel_the_active_bet from M-package M9 have ended on line "'.$e->getLine().'" on file "'.$e->getFile().'" with error: '.$e->getMessage();
