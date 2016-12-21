f1976431e8e4d3c6d432b649f554593129893c7b
4gekkman <4gekkman@gmail.com>
2016-12-21 16:37:31 +0300

    Autosave to

---
+++ view.blade.php
@@ -0,0 +1,91 @@
+@extends($layoutid)
+<?php /*=====================================================////
+////																											   ////
+////                    Документ D-пакета      				       ////
+////																												 ////
+////========================================================*/ ?>
+
+
+<?php /*------------------->
+<!-- 1. Title документа  -->
+<!---------------------*/ ?>
+@section('title')
+
+  <title>D10009 - CSGOHAP main document</title>
+
+@stop
+
+
+
+<?php /*----------------->
+<!-- 2. CSS документа  -->
+<!-------------------*/ ?>
+@section('css')
+
+  <!-- document css: start -->
+  <link rel="stylesheet" type="text/css" href="{!! (\Request::secure() ? "https://" : "http://") . (\Request::getHost()) . ":" . (\Request::getPort()); !!}/public/bower/4gekkman-bower-cssgrids/c.css?rand={!! mt_rand(1000,9999); !!}">
+  <link rel="stylesheet" type="text/css" href="{!! (\Request::secure() ? "https://" : "http://") . (\Request::getHost()) . ":" . (\Request::getPort()); !!}/public/L10003/css/c.css?rand={!! mt_rand(1000,9999); !!}">
+  <link rel="stylesheet" type="text/css" href="{!! (\Request::secure() ? "https://" : "http://") . (\Request::getHost()) . ":" . (\Request::getPort()); !!}/public/D10009/css/c.css?rand={!! mt_rand(1000,9999); !!}">
+  <!-- document css: stop -->
+
+@stop
+
+
+<?php /*--------------------->
+<!-- 3. Контент документа  -->
+<!--------------------------->
+Оглавление
+
+
+
+
+
+-------------------------*/ ?>
+@section('content')
+
+  Контент документа
+
+@stop
+
+
+
+<?php /*--------------------------------------------------------------->
+<!-- 4. Получение данных с сервера и подключение JS этого документа  -->
+<!-----------------------------------------------------------------*/ ?>
+@section('js')
+
+  <?php /*-------------------------------->
+  <!-- 4.1. Получение данных с сервера  -->
+  <!----------------------------------*/ ?>
+  <script>
+
+    // 1. Подготовить объект, в который будут записаны данные
+    var server = {};
+
+    // 2. Принять данные
+
+      // 2.1. Принять csrf_token
+      server.csrf_token             = "{{ csrf_token() }}";
+
+      // 2.2. Принять переданные из контроллера данные
+      server.data                   =  {!! $data !!};
+
+  </script>
+
+
+  <?php /*------------------------------------>
+  <!-- 4.2. Подключение JS этого документа  -->
+  <!--------------------------------------*/ ?>
+
+  <!-- document js: start -->
+  <script src="{!! (\Request::secure() ? "https://" : "http://") . (\Request::getHost()) . ":" . (\Request::getPort()); !!}/public/bower/4gekkman-bower-jslib1/library.js?rand={!! mt_rand(1000,9999); !!}"></script>
+  <script src="{!! (\Request::secure() ? "https://" : "http://") . (\Request::getHost()) . ":" . (\Request::getPort()); !!}/public/L10003/js/j.js?rand={!! mt_rand(1000,9999); !!}"></script>
+  <script src="{!! (\Request::secure() ? "https://" : "http://") . (\Request::getHost()) . ":" . (\Request::getPort()); !!}/public/D10009/js/j.js?rand={!! mt_rand(1000,9999); !!}"></script>
+  <!-- document js: stop -->
+
+
+@stop
+
+
+
+
