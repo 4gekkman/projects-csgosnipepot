e9b5408828246fe7c767a494a2e60e83dcba31fc
4gekkman <4gekkman@gmail.com>
2017-03-01 12:10:21 +0300

    Autosave to

---
+++ ServiceProvider.php
@@ -0,0 +1,128 @@
+<?php 
+////========================================================////
+////																											  ////
+////               Сервис-провайдер M-пакета					      ////
+////																												////
+////========================================================////
+
+
+  //-------------------------------------//
+  // Пространство имён сервис-провайдера //
+  //-------------------------------------//
+
+    namespace M13;
+
+
+  //---------------------------------//
+  // Подключение необходимых классов //
+  //---------------------------------//
+  use Illuminate\Support\ServiceProvider as BaseServiceProvider,
+      Illuminate\Contracts\Events\Dispatcher as DispatcherContract,
+      Illuminate\Support\Facades\Validator,
+      Illuminate\Support\Facades\Event;
+
+
+  //------------------//
+  // Сервис-провайдер //
+  //------------------//
+  class ServiceProvider extends BaseServiceProvider {
+
+    //------//
+    // Boot //
+    //------//
+    public function boot(DispatcherContract $events, \Illuminate\Contracts\Http\Kernel $kernel) {
+
+      //--------------//
+      // 1. Параметры //
+      //--------------//
+
+        // 1] ID M-пакета //
+        //----------------//
+        $packid = "M13";
+
+        // 2] Пары 'событие-обработчик' документов M-пакета //
+        //--------------------------------------------------//
+        $pairs2register = [
+
+        ];
+
+      //----------------------------------//
+      // 2. Код (руками не редактировать) //
+      //----------------------------------//
+
+        // 1] Публикация настроек M-пакета в config проекта //
+        //--------------------------------------------------//
+        if(!file_exists(config_path($packid.'.php'))) {
+          $this->publishes([
+              __DIR__.'/settings.php' =>
+              config_path($packid.'.php'),
+          ], 'M');
+        }
+
+        // 2] Регистрация Before- и After-middleware M-пакета //
+        //----------------------------------------------------//
+        $this->app['router']->pushMiddlewareToGroup('web', $packid.'\Middlewares\AfterMiddleware');
+        $this->app['router']->pushMiddlewareToGroup('web', $packid.'\Middlewares\BeforeMiddleware');
+
+        // 3] Регистрация пар 'событие-обработчик' документом M-пакета документа "" //
+        //--------------------------------------------------------------------------//
+        foreach($pairs2register as $handler => $event) {
+          $events->listen($event, $handler);
+        }
+
+        // 4] Регистрация файлов локализации D-пакета //
+        //--------------------------------------------//
+        // - Доступ из php  : trans('m1.welcome')
+        // - Доступ из blade: {{ trans('m1.welcome') }}
+
+          // RU //
+          //----//
+          $this->publishes([
+              __DIR__.'/Localization/ru/localization.php' => resource_path('lang/ru/'.$packid.'.php'),
+          ], 'M');
+
+          // EN //
+          //----//
+          $this->publishes([
+              __DIR__.'/Localization/en/localization.php' => resource_path('lang/en/'.$packid.'.php'),
+          ], 'M');
+
+    }
+
+    //--------------------------------------------------//
+    // Register - регистрация связей в сервис-контейнер //
+    //--------------------------------------------------//
+    public function register()
+    {
+
+      //-----------------------------------------------------//
+      // 1. Добавление консольных команд в планировщик задач //
+      //-----------------------------------------------------//
+
+        // Список подготовленных для добавление в планировщик строк
+        // - Статья про cron-формат: http://www.nncron.ru/help/EN/working/cron-format.htm
+        // - Примеры:
+        //
+        //    $schedule->command("m1:parseapp")->withoutOverlapping()->hourly();                        // каждый час
+        //    $schedule->command("m1:parseapp")->withoutOverlapping()->cron("0,15,30,45 * * * * *");    // каждые 15 минут
+        //
+        $add2schedule = [
+
+        ];
+
+      //----------------------------------------------------//
+      // 2. Регистрация консольных команд документов модуля //
+      //----------------------------------------------------//
+
+        // Список команд для регистрации
+        // Пример: '\M1\Console\T1_parseapp',
+        $commands = [
+
+        ];
+
+        // Регистрация команд в методе register
+        $this->commands($commands);
+
+    }
+
+  }
